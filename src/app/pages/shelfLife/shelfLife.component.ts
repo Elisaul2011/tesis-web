0003e8910fa935c5ac6d439a1cd03e5c4f9ce0bf
Eduardo <eduardokunmixyeah@gmail.com>
2024-08-12 03:31:08 -0400

    complete shelft life

--- src/app/pages/shelfLife/shelfLife.component.ts
+++ src/app/pages/shelfLife/shelfLife.component.ts
@@ -9,6 +9,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { ReportSlService } from '../../services/report-sl.service';
 import { BodyCreateTaller, ITaller, BodyUpdateTaller } from '../../interfaces/taller';
 import { IShelfLife } from '../../interfaces/shelfLife';
+import { IInventario } from '../../interfaces/inventario';
 
 @Component({
   selector: 'app-shelf-life',
@@ -23,8 +24,8 @@ import { IShelfLife } from '../../interfaces/shelfLife';
 })
 export class ShelfLifeComponent {
 
-  columnsShelfLife: IColumns<IShelfLife>[] = columnsShelfLife;
-  dataSL: IShelfLife[] = [];
+  columnsShelfLife: IColumns<IInventario>[] = columnsShelfLife;
+  dataSL: IInventario[] = [];
   SLConfigTable: IConfigTable={btnAdd: false}
   reportSLService = inject(ReportSlService)
 
@@ -39,47 +40,4 @@ export class ShelfLifeComponent {
   ngOnInit(): void {
     this.reportSLService.getReportSL();
   }
-
-  defectColumnAction(dataComponent: ISendDataTable): void {
-    if(dataComponent.action == 'add'){
-      this.openDialog();
-    }
-    if(dataComponent.action == 'edit'){
-      this.editDataDialog(dataComponent.data);
-    }
-  }
-
-  openDialog(): void {
-    formularioShelfLife.dataForm.map(form => form.value = '');
-
-    const dialogRef = this.dialog.open(FormularioComponent, {
-      panelClass: 'stylesDialog',
-      data: formularioShelfLife,
-    });
-
-    dialogRef.afterClosed().subscribe((result:BodyCreateTaller ) => {
-      this.reportSLService.postReport(result)
-    })
-  }
-
-  editDataDialog(data: ITaller): void {
-    formularioShelfLife.dataForm.map(form => {
-      const findByName = formularioShelfLife.dataForm.find(loquesea => loquesea.formControl == form.formControl);
-      if(findByName){
-        findByName.value = data[form.formControl as keyof ITaller]
-      }
-    })
-
-    const dialogRef = this.dialog.open(FormularioComponent, {
-      data: formularioShelfLife,
-    });
-
-    dialogRef.afterClosed().subscribe((result: BodyUpdateTaller) => {
-      if(result){
-        result.idTaller = data.idTaller;
-        this.reportSLService.putReport(result)
-      }
-    })
-  }
-
 }
